e80cdd57c69ca62d64815f0585a5125f1ab7f3ff
Macbook <macbook@MacBook-Pro-Macbook.local>
2019-10-06 17:25:44 +0300

    +  выбор размера сдвига

--- ceaser.py
+++ ceaser.py
@@ -1,23 +1,23 @@
-def encrypt_ceasar(sampletext: str) -> str:
+def encrypt_caesar(sampletext: str,shift: int) -> str:
     result = ""
     for char in sampletext: 
         code = ord(char)
-        code = code + 3
+        code = code + shift
         if(code > 90):
             code = code - 26
         nchar = chr(code)
         result += nchar
     return result
 
-def decrypt_caesar(ciphertext: str) -> str:
+def decrypt_caesar(ciphertext: str, shift: int) -> str:
     result = ""
     for char in ciphertext: 
         code = ord(char)
-        code = code - 3
+        code = code - shift
         if(code < 65):
             code = code + 26
         nchar = chr(code)
         result += nchar
     return result
 
-print(decrypt_caesar("SBWKRQ"))
+print(decrypt_caesar(encrypt_caesar("PYTHON", 6 ),6))
